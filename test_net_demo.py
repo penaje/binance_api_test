8ae75db9ff610d85978efefd2593d4320d2f8de7
penaje <penaje@oregonstate.edu>
2022-10-09 15:38:55 -0700

    testing

--- test_net_demo.py
+++ test_net_demo.py
@@ -3,3 +3,40 @@
 # API Key: sxV243LlOdPF8hzKGfiZWjnN4PZAfwkQRpNDnYlqHYHmWh8HGqRQlMCxzLjYMxb7
 
 # Secret Key: B9duqs0DkZE2wQqohygM7IxHleZsgZ3ov7o6ObmWMZz8me8FBFfHg7Ys4Sdagnvs
+
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance.enums import *
+from time import sleep
+import json
+
+
+
+api_key = 'sxV243LlOdPF8hzKGfiZWjnN4PZAfwkQRpNDnYlqHYHmWh8HGqRQlMCxzLjYMxb7'
+api_private = 'B9duqs0DkZE2wQqohygM7IxHleZsgZ3ov7o6ObmWMZz8me8FBFfHg7Ys4Sdagnvs'
+
+client = Client(api_key, api_private, testnet=True, tld='us')
+
+btc_balance = client.get_asset_balance(asset='BTC')
+
+print(btc_balance)
+
+btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
+print(btc_price["price"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
