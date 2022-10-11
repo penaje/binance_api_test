831f2b53e693499c5a5614db7237c72f38f77bef
penaje <penaje@oregonstate.edu>
2022-10-10 21:41:48 -0700

    something new

--- test_net_demo.py
+++ test_net_demo.py
@@ -1,42 +1,19 @@
-# Let's try this out
-
-# API Key: sxV243LlOdPF8hzKGfiZWjnN4PZAfwkQRpNDnYlqHYHmWh8HGqRQlMCxzLjYMxb7
-
-# Secret Key: B9duqs0DkZE2wQqohygM7IxHleZsgZ3ov7o6ObmWMZz8me8FBFfHg7Ys4Sdagnvs
-
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 from binance.enums import *
 from time import sleep
 import json
 
-
-
 api_key = 'sxV243LlOdPF8hzKGfiZWjnN4PZAfwkQRpNDnYlqHYHmWh8HGqRQlMCxzLjYMxb7'
-api_private = 'B9duqs0DkZE2wQqohygM7IxHleZsgZ3ov7o6ObmWMZz8me8FBFfHg7Ys4Sdagnvs'
-
-client = Client(api_key, api_private, testnet=True, tld='us')
-
-btc_balance = client.get_asset_balance(asset='BTC')
-
-print(btc_balance)
-
-btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
-print(btc_price["price"])
+api_secret = 'B9duqs0DkZE2wQqohygM7IxHleZsgZ3ov7o6ObmWMZz8me8FBFfHg7Ys4Sdagnvs'
+client = Client(api_key, api_secret, testnet=True, tld='us')
 
 
+class TradeBot:
+    """Trading Bot Class"""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def __init__(self, bot_name):
+        # Probably don't need a name but oh well
+        self.name = bot_name
+        self._btc_balance = None
+        self._last_btc_price = None
 
